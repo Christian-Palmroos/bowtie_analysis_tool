84b14ec7017c4def22ff1eb73201c0c807bcff08
Christian-Palmroos <chospa@utu.fi>
2024-12-02 11:47:46 +0200

    First commit

---
+++ Bowtie/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+import sys
+
+from matplotlib import pyplot as plt
+
+from . import bowtie_util as btu
+
+if __name__ == "__main__":
+
+    print(":D")
